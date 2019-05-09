76c69ecd225a135e72ff689d6daecc9c3571724b
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2019-05-09 08:57:00 -0600

    dunham do this

--- KillPhotoshop/src/KillPhotoshopRunner.java
+++ KillPhotoshop/src/KillPhotoshopRunner.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class KillPhotoshopRunner
 	{
+		public static Picture p;
 		//menu in the console
 		public static void main(String[] args)
 			{
@@ -15,11 +16,49 @@ public class KillPhotoshopRunner
 		public static void menu()
 		{
 			Scanner userInt = new Scanner(System.in);
-			System.out.println("1) Mirror Horizontally");
+			System.out.println("Please choose a picture");
+			System.out.println("1) a beautiful arch");
+			System.out.println("2) a nice beach");
+			System.out.println("3) blobfish");
+			System.out.println("4) moped");
+			System.out.println("5) butterfly");
+			System.out.println("6) caterpillar");
+			System.out.println("7) China");
+			System.out.println("8) gorge");
+			System.out.println("9) koala");
 			int userAnswer = userInt.nextInt();
-			if (userAnswer == 1){
-				PictureTester.testmirrorHorizontal();
+			switch(userAnswer) {
+				case 1: p = new Picture("arch.jpg");
+				break;
+				
+				case 2: p = new Picture("beach.jpg");
+				break;
+				
+				case 3: p = new Picture("blobfish.jpg");
+				break;
+				
+				case 4: p = new Picture("motorcycle.jpg");
+				break;
+				
+				case 5: p = new Picture("butterfly.jpg");
+				break;
+				
+				case 6: p = new Picture("caterpillar.jpg");
+				break;
+				
+				case 7: p = new Picture("china.jpg");
+				break;
+				
+				case 8: p = new Picture("gorge.jpg");
+				break;
+				
+				case 9: p = new Picture("koala.jpg");
+				break;
+				
+				case 10: p = new Picture("beach.jpg");
+				break;
 			}
+			p.explore();
 		}
 
 	}
